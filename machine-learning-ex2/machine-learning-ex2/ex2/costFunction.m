abe44ed322d6a92add13368c3b79441d4286ef05
Samir Rehmtulla <s2rehmtu@uwaterloo.ca>
2018-03-05 21:07:27 -0500

    Completed week 3, assignment on logistic regression and regularization

---
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
@@ -0,0 +1,32 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+constant = -1.0 / m;
+linear = X * theta;
+prediction = sigmoid(linear);
+SummationTerms = y' * log(prediction) + (1-y)' *log(1-prediction);
+J = constant*sum(SummationTerms);
+
+
+grad = (X' * (prediction - y))/m;
+
+
+
+
+
+
+
+
+% =============================================================
+
+end
